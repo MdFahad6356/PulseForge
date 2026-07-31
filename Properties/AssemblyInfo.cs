9d73735e96b02e024d2045a9ce9090514e8f2390
Md Ataullah Al Mahmud <36507008+MdFahad6356@users.noreply.github.com>
2026-08-01 00:39:13 +1000

    Add assembly metadata

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PulseForge")]
+[assembly: AssemblyDescription("A controlled Windows CPU and memory stability lab")]
+[assembly: AssemblyCompany("Independent project")]
+[assembly: AssemblyProduct("PulseForge")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f7c5ee49-0277-4f92-9e97-2142ba750c5a")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
